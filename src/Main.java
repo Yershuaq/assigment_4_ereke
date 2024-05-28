4cd2362a7daa472bfd256eee76c8628366571fd5
solta <230930@astanait.edu.kz>
2024-05-29 00:11:11 +0500

    my Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Class.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        WeightedGraph<String> graph = new WeightedGraph<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        graph.addEdge("Almaty", "Astana", 1);
+        graph.addEdge("Astana", "Shymkent", 2);
+        graph.addEdge("Shymkent", "Kyzylorda", 3);
+        graph.addEdge("Almaty", "Shymkent", 2);
+        graph.addEdge("Astana", "Atyrau", 5);
+
+        DijkstraSearch<String> dijkstra = new DijkstraSearch<>(graph, "Almaty");
+        BreadthFirstSearch<String> bfs = new BreadthFirstSearch<>(graph, "Almaty");
+
+        System.out.println("Dijkstra:");
+        System.out.println(String.join(" -> ", dijkstra.getPathTo("Kyzylorda")));
+        System.out.println("--------------------------------");
+        System.out.println("BFS:");
+        System.out.println(String.join(" -> ", bfs.getPathTo("Kyzylorda")));
+        System.out.println("--------------------------------");
     }
 }
