0218e885e3b76f7bf150e158495662a6f3c6fd6c
solta <230930@astanait.edu.kz>
2024-05-29 00:23:22 +0500

    BreadthFirstSearch.java финальный код

--- src/Class/BreadthFirstSearch.java
+++ src/Class/BreadthFirstSearch.java
@@ -3,39 +3,39 @@ package Class;
 import java.util.*;
 
 public class BreadthFirstSearch<V> extends Search<V> {
-    public BreadthFirstSearch(WeightedGraph<V> graph, V startVertex) {
+    public BreadthFirstSearch(WeightedGraph<V> graph, Vertex<V> startVertex) {
         super(graph, startVertex);
     }
 
     @Override
-    public List<V> getPathTo(V endVertex) {
-        Map<V, V> parentMap = new HashMap<>();
-        Queue<V> queue = new LinkedList<>();
-        Set<V> visited = new HashSet<>();
+    public List<V> getPathTo(Vertex<V> endVertex) {
+        Map<Vertex<V>, Vertex<V>> parentMap = new HashMap<>();
+        Queue<Vertex<V>> queue = new LinkedList<>();
+        Set<Vertex<V>> visited = new HashSet<>();
 
         queue.add(startVertex);
         visited.add(startVertex);
 
         while (!queue.isEmpty()) {
-            V current = queue.poll();
+            Vertex<V> current = queue.poll();
             if (current.equals(endVertex)) {
                 break;
             }
 
-            for (Vertex<V> neighbor : graph.getVertex(current).getAdjacentVertices().keySet()) {
-                if (!visited.contains(neighbor.getData())) {
-                    visited.add(neighbor.getData());
-                    parentMap.put(neighbor.getData(), current);
-                    queue.add(neighbor.getData());
+            for (Vertex<V> neighbor : current.getAdjacentVertices().keySet()) {
+                if (!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    parentMap.put(neighbor, current);
+                    queue.add(neighbor);
                 }
             }
         }
 
         List<V> path = new LinkedList<>();
-        V step = endVertex;
+        Vertex<V> step = endVertex;
         if (parentMap.containsKey(step) || step.equals(startVertex)) {
             while (step != null) {
-                path.add(step);
+                path.add(step.getData());
                 step = parentMap.get(step);
             }
             Collections.reverse(path);
